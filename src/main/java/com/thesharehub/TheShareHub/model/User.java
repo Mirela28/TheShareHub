175566db3258f422f3384ad9b011877d291a3728
Mirela28 <mirelagirleanu@gmail.com>
2025-10-08 14:31:07 +0200

    Authentication Refactor

--- src/main/java/com/thesharehub/TheShareHub/model/User.java
+++ src/main/java/com/thesharehub/TheShareHub/model/User.java
@@ -32,6 +32,8 @@ public class User {
         city = _city;
     }
 
-    public User(String username, String password) {
+    public User(String _username, String _password) {
+        username = _username;
+        password = _password;
     }
 }

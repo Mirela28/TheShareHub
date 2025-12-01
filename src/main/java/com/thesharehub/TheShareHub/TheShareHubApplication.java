a00983195fd61d09ea7f57ff48aa1d8a9e8c64d0
Mirela28 <mirelagirleanu@gmail.com>
2025-12-01 18:41:51 +0100

    Stripe Payment

--- src/main/java/com/thesharehub/TheShareHub/TheShareHubApplication.java
+++ src/main/java/com/thesharehub/TheShareHub/TheShareHubApplication.java
@@ -1,13 +1,35 @@
 package com.thesharehub.TheShareHub;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
 
 @SpringBootApplication
 public class TheShareHubApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TheShareHubApplication.class, args);
+        if (System.getProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME) == null &&
+                System.getenv("SPRING_PROFILES_ACTIVE") == null) {
+            System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "dev");
+        }
+
+        String activeProfile = System.getProperty("spring.profiles.active",
+                System.getenv("SPRING_PROFILES_ACTIVE"));
+
+        if ("dev".equals(activeProfile)) {
+            Dotenv dotenv = Dotenv.configure()
+                    .ignoreIfMissing()
+                    .load();
+
+            dotenv.entries().forEach(entry -> {
+                if (System.getProperty(entry.getKey()) == null) {
+                    System.setProperty(entry.getKey(), entry.getValue());
+                }
+            });
+        }
+
+        SpringApplication.run(TheShareHubApplication.class, args);
 	}
 
 }

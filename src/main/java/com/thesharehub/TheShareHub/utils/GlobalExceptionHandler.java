ca52f08125edb39550762e838e95c32962571442
Mirela28 <mirelagirleanu@gmail.com>
2025-12-26 14:38:22 +0100

    Final commit integration test

--- src/main/java/com/thesharehub/TheShareHub/utils/GlobalExceptionHandler.java
+++ src/main/java/com/thesharehub/TheShareHub/utils/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.HashMap;
 import java.util.List;
@@ -40,6 +41,12 @@ public class GlobalExceptionHandler {
         response.put("errors", List.of(ex.getMessage()));
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<?> handleResponseStatus(ResponseStatusException ex) {
+        return ResponseEntity.status(ex.getStatusCode())
+                .body(Map.of("error", ex.getReason()));
     }
 
 
